9b3b661b13168d771b6ff37706d558c571a03794
Buterbrodik <tima.fayzullin.03@inbox.ru>
2023-06-16 12:06:45 +0500

    я перевернул строку и проверил совпадает ли она с начальной строкой

---
+++ dz_lesson1.py
@@ -0,0 +1,11 @@
+def check(string):
+    ls = []
+    for i in string[::-1]:
+        ls.append(i)
+        str_palindrom = ''.join(ls)
+    if string == str_palindrom:
+        print('True')
+    else:
+        print('False')
+check('helleh')
+check('hello')
