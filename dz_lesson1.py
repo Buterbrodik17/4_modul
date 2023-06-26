514a3c7fcc4af81d040175bdff44de1f4026f10e
Buterbrodik <tima.fayzullin.03@inbox.ru>
2023-06-26 13:40:30 +0500

    html and css

--- dz_lesson1.py
+++ dz_lesson1.py
@@ -1,11 +1,16 @@
-def check(string):
-    ls = []
-    for i in string[::-1]:
-        ls.append(i)
-        str_palindrom = ''.join(ls)
-    if string == str_palindrom:
-        print('True')
-    else:
-        print('False')
-check('helleh')
-check('hello')
+# def check(string):
+#     ls = []
+#     for i in string[::-1]:
+#         ls.append(i)
+#         str_palindrom = ''.join(ls)
+#     if string == str_palindrom:
+#         print('True')
+#     else:
+#         print('False')
+# check('helleh')
+# check('hello')
+str = 'helleh'
+if str == str[::-1]:
+    print('True')
+else:
+    print('False')
