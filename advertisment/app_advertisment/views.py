0b1a0621c4753416df026fd241ed1bbc95009495
Buterbrodik <tima.fayzullin.03@inbox.ru>
2023-07-14 14:57:12 +0500

    5 урок дз

--- advertisment/app_advertisment/views.py
+++ advertisment/app_advertisment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h1>Это мой первый сайт!!!</h1>')
+    return render(request, 'index.html')
 
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
 # Create your views here.
