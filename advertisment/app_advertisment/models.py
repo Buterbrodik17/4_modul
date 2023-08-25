c2a5fb4801afb9cd1ad08ae617f66fd479cc8015
Buterbrodik <tima.fayzullin.03@inbox.ru>
2023-08-25 14:38:28 +0500

    4 задание 10 урок

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -8,20 +8,19 @@ User = get_user_model()
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
     description = models.TextField('Описание')
-    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2,)
     auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    image = models.ImageField('Изображение', upload_to='advertisements/')
+    image = models.ImageField('Изображение', upload_to='advertisements/', null=True, blank=True)
 
     @admin.display(description='Дата создания')
     def created_date(self):
         if self.created_at.date() == timezone.now().date():
             created_time = self.created_at.time().strftime('%H:%M:%S')
-            return format_html(
-                '<span>Сегодня в {}</span>', created_time
-            )
+            self.html = format_html('<span>Сегодня в {}</span>', created_time)
+            return self.html
         return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
 
     @admin.display(description='Дата обновления')
